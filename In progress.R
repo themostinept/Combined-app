5d7818bac54c6c9248ea0032d16f9a3f28af45b3
tician_a <tician.an@big3.ru>
2019-09-26 18:31:56 +0300

    dividing bounding box

--- In progress.R
+++ In progress.R
@@ -1,14 +1,30 @@
+library(gtools)
 y <- '58.622468, 31.406503'
 z <- '58.461637, 31.118112'
-coord1 <- unlist(strsplit(trimws(y), split = ","))
-coord2 <- unlist(strsplit(trimws(z), split = ","))
-coord1 <- as.numeric(coord1)
-coord2 <- as.numeric(coord2)
-s1 <- seq(from = coord2[1], to = coord1[1], length.out = 6)
-s2 <- seq(from = coord2[2], to = coord1[2], length.out = 6)
-s <- paste(s1, s2)
-s1 <- s1[-length(s1)]
-s2 <- s2[-length(s2)]
+make_grid <- function(ru, ld) {
+  coord1 <- as.numeric(unlist(strsplit(trimws(ru), split = ",")))
+  coord2 <- as.numeric(unlist(strsplit(trimws(ld), split = ",")))
+  s1 <- seq(from = coord2[1], to = coord1[1], length.out = 6)
+  s2 <- seq(from = coord2[2], to = coord1[2], length.out = 6)
+  ss <- expand.grid(s1, s2)
+  ss <- ss %>% 
+    unite(col = coords, Var1, Var2, sep = ", ") %>% 
+    mutate(n = rep(1:(length(s1)), length(s1)))
+coords <- matrix(nrow = 5, ncol = 5)
+for(i in 1:max(ss[, 2])) {
+      if (i + 1 < max(ss[, 2]) + 1) {
+        lcv <- ss[which(ss[, 2] == i), 1]
+        lcv <- lcv[-length(lcv)]
+        rcv <- ss[which(ss[, 2] == (i + 1)), 1]
+        rcv <- rcv[-1]
+        n <- i
+        coords[, i] <- paste(lcv, rcv, sep = "_")
+      }
+  }
+  return(coords)
+}
+as.data.frame(do.call(rbind, res))
+
 x <- 'АЗС'
 apikey <- '3d382e5a-4cbd-4f19-86e0-112a084057a9'
 coords <- paste(s11, s22, sep = "_")
