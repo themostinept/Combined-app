38ad8f9587c5bf393f6d795116a8474dd0144fab
themostinept <oren-konsp@yandex.ru>
2019-10-06 14:37:52 +0300

    first successful test

--- In progress.R
+++ In progress.R
@@ -1,8 +1,8 @@
 
 ru <- '58.622468, 31.406503'
 ld <- '58.461637, 31.118112'
-height <- 3
-width <- 2
+height <- 1
+width <- 3
 make_grid <- function(ru, ld, height, width) {
   coord1 <- as.numeric(unlist(strsplit(trimws(ru), split = ",")))
   coord2 <- as.numeric(unlist(strsplit(trimws(ld), split = ",")))
@@ -28,49 +28,40 @@ make_grid <- function(ru, ld, height, width) {
 }
 result <- data.frame()
 for (i in 1:length(coords)) {
-  res <- yandex_geosearch_bb(x, coords[i], apikey)
+  res <- yandex_geosearch_bb(search_line, coords[i], apikey)
   result <- rbind(result, res)
+  rm(res)
 }
 
-x <- 'АЗС'
+search_line <- 'АЗС'
 apikey <- '3d382e5a-4cbd-4f19-86e0-112a084057a9'
 
 
-yandex_geosearch_bb <- function(x, coords, apikey, export = F) {
-  require(jsonlite)
-  require(openxlsx)
+yandex_geosearch_bb <- function(serach_line, coords, apikey) {
   coords <- unlist(strsplit(trimws(coords), split = "_"))
-  
   #First, prepare request phrase and convert coordinates from 'lat, lon' into 'lon, lat' format
-  request <- URLencode(enc2utf8(x))
+  request <- URLencode(enc2utf8(search_line))
   coord1 <- unlist(strsplit(trimws(coords[1]), split = ","))
   coord1 <- paste(coord1[2], coord1[1], sep = ",")
   coord2 <- unlist(strsplit(trimws(coords[2]), split = ","))
   coord2 <- paste(coord2[2], coord2[1], sep = ",")
-  
   #Combine a complete url for request  
   first_part <- "https://search-maps.yandex.ru/v1/?apikey="
   url_compl <- gsub(" ", "", paste(first_part, apikey, "&text=", request, "&type=biz&lang=ru_RU&", "bbox=", coord1, "~", coord2, "&results=500", collapse = ""))
-  
   #Obtain a request results in json format  
   full_req <- suppressWarnings(fromJSON(paste(readLines(url_compl, encoding = 'UTF-8'), collapse="")))
-  
   #Desired results are stored in element, called 'feautures'. Here we take from there only name, address, url and coordinates of an object 
   req_data <- full_req$features
   if (length(req_data) < 1) {
-    return('Nothing to show')
+    return(NULL)
   }
   prop <- req_data$properties
   geo <- req_data$geometry
   vec_geo <- unlist(geo$coordinates)
   geo_df <- data.frame(lat = vec_geo[seq(2,length(vec_geo), by = 2)], lon = vec_geo[seq(1,length(vec_geo), by = 2)])
-  
-  #Combine our vectors in a dataframe and save it as csv if 'export' argument was set to 'TRUE'
+  #Combine our vectors in a dataframe
   total <- cbind(prop$name, prop$description, ifelse(is.null(prop$CompanyMetaData$url) == TRUE, rep(NA, length(prop$name)), prop$CompanyMetaData$url), geo_df)
   colnames(total) <- c("Name", "Address", "URL", "Lat", "Lon")
-  if (export == TRUE) {
-    write.xlsx(total, 'request_api_result.xlsx')
-  }
   return(total)
 }
 
--- app.R
+++ app.R
@@ -5,6 +5,60 @@ library(maptools)
 library(jsonlite)
 library(openxlsx)
 crswgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+yandex_geosearch_bb <- function(search_req, coords, apikey) {
+  coords <- unlist(strsplit(trimws(coords), split = "_"))
+  #First, prepare request phrase and convert coordinates from 'lat, lon' into 'lon, lat' format
+  request <- URLencode(enc2utf8(search_req))
+  coord1 <- unlist(strsplit(trimws(coords[1]), split = ","))
+  coord1 <- paste(coord1[2], coord1[1], sep = ",")
+  coord2 <- unlist(strsplit(trimws(coords[2]), split = ","))
+  coord2 <- paste(coord2[2], coord2[1], sep = ",")
+  #Combine a complete url for request  
+  first_part <- "https://search-maps.yandex.ru/v1/?apikey="
+  url_compl <- gsub(" ", "", paste(first_part, apikey, "&text=", request, "&type=biz&lang=ru_RU&", "bbox=", coord1, "~", coord2, "&results=500", collapse = ""))
+  #Obtain a request results in json format  
+  full_req <- suppressWarnings(fromJSON(paste(readLines(url_compl, encoding = 'UTF-8'), collapse="")))
+  #Desired results are stored in element, called 'feautures'. Here we take from there only name, address, url and coordinates of an object 
+  req_data <- full_req$features
+  if (length(req_data) < 1) {
+    return(NULL)
+  }
+  prop <- req_data$properties
+  geo <- req_data$geometry
+  vec_geo <- unlist(geo$coordinates)
+  geo_df <- data.frame(lat = vec_geo[seq(2,length(vec_geo), by = 2)], lon = vec_geo[seq(1,length(vec_geo), by = 2)])
+  #Combine our vectors in a dataframe
+  total <- cbind(prop$name, prop$description, ifelse(is.null(prop$CompanyMetaData$url) == TRUE, rep(NA, length(prop$name)), prop$CompanyMetaData$url), geo_df)
+  colnames(total) <- c("Name", "Address", "URL", "Lat", "Lon")
+  return(total)
+}
+make_grid <- function(ru, ld, height, width) {
+  if (height > 1 || width > 1) {
+    coord1 <- as.numeric(unlist(strsplit(trimws(ru), split = ",")))
+    coord2 <- as.numeric(unlist(strsplit(trimws(ld), split = ",")))
+    s1 <- seq(from = coord2[1], to = coord1[1], length.out = height + 1)
+    s2 <- seq(from = coord2[2], to = coord1[2], length.out = width + 1)
+    ss <- expand.grid(s1, s2)
+    ss <- ss %>% 
+      unite(col = coords, Var1, Var2, sep = ", ") %>% 
+      mutate(n = rep(1:(length(s1)), length(s2)))
+    coords <- matrix(nrow = width, ncol = height)
+    for(i in 1:max(ss[, 2])) {
+      if (i + 1 < max(ss[, 2]) + 1) {
+        lcv <- ss[which(ss[, 2] == i), 1]
+        lcv <- lcv[-length(lcv)]
+        rcv <- ss[which(ss[, 2] == (i + 1)), 1]
+        rcv <- rcv[-1]
+        n <- i
+        coords[, i] <- paste(lcv, rcv, sep = "_")
+      }
+    }
+    coords <- matrix(coords, ncol = 1)
+    } else {
+    coords <- paste(ld, ru, sep = "_")
+  }
+  return(coords)
+}
 
 #Элементы пользовательского интерфейса
 ui <- tagList(
@@ -42,25 +96,25 @@ ui <- tagList(
                  Координаты должны передаваться в том же виде, в котором их выдают Яндекс-карты.
                  Высота и ширина области поиска подбираются интуитивно."),
         hr(),
-        textInput("search_line", label = h4("Введите запрос")),
+        textInput("search_line", label = h4("Введите запрос"), value = "Аптека"),
         hr(),
         fluidRow(column(4)),
-        textInput("key_line", label = h4("Введите api-ключ")),
+        textInput("key_line", label = h4("Введите api-ключ"), value = '3d382e5a-4cbd-4f19-86e0-112a084057a9'),
         hr(),
         fluidRow(column(2)),
-        textInput("coordru_line", label = h4("Введите координаты области поиска: сначала верхнюю правую, затем нижнюю левую")),
-        textInput("coordld_line", label = h4()),
+        textInput("coordru_line", label = h4("Введите координаты области поиска: сначала верхнюю правую, затем нижнюю левую"), value = '58.622468, 31.406503'),
+        textInput("coordld_line", label = h4(), value = '58.461637, 31.118112'),
         hr(),
         fluidRow(column(4)),
-        numericInput("num_line", label = h4("Укажите высоту разбивки области поиска"), 3),
+        numericInput("num_line_h", label = h4("Укажите высоту разбивки области поиска"), 1),
         fluidRow(column(4)),
-        numericInput("num_line", label = h4("Укажите ширину разбивки области поиска"), 3),
+        numericInput("num_line_w", label = h4("Укажите ширину разбивки области поиска"), 2),
         hr(),
         fluidRow(column(4)),
-        checkboxInput("checkbox", label = "Проверять координаты на попадание в границы региона", value = TRUE),
+        checkboxInput("checkbox_oktmo", label = "Проверять координаты на попадание в границы региона", value = FALSE),
         hr(),
         fluidRow(column(3)),
-        downloadButton("Download", label = "Скачать результат")
+        downloadButton("Download_yandex_search", label = "Скачать результат")
       )
     )
   )
@@ -143,6 +197,57 @@ server <- function(input, output) {
   )
 
 #API Яндекса для поиска организаций
+  #Записываем входные параметры
+  ru <- reactive({
+    input$coordru_line
+  })
+  ld <- reactive({
+    input$coordld_line
+  })
+  height <- reactive({
+    input$num_line_h
+  })
+  width <- reactive({
+    input$num_line_w
+  })
+  search_req <- reactive({
+    input$search_line
+  })
+  apikey <- reactive({
+    input$key_line
+  })
+  check_oktmo <- reactive({
+    input$checkbox_oktmo
+  })
+  #Разбиваем область поиска на блоки
+  coords <- reactive({make_grid(ru(), ld(), height(), width())})
+  #Сохраняем результаты группы запросов в один датафрейм
+  result_ya <- reactive({
+    result <- data.frame()
+    for (i in 1:length(coords())) {
+      res <- yandex_geosearch_bb(search_req(), coords()[[i]], apikey())
+      result <- rbind(result, res)
+      rm(res)
+    }
+  #Проверяем (или нет) координаты на попадание в границы и проставляем ОКТМО
+    if (check_oktmo() == TRUE) {
+      m <- matrix(c(result$Lon, result$Lat), ncol = 2)
+      points_polygon <- SpatialPoints(m, proj4string = crswgs84)
+      result <- cbind(result,(over(points_polygon, map_shp())))
+      return(result)
+    } else {
+      return(result)
+    }
+  })  
+  #Вывод результата в формате xlsx через кнопку загрузки
+  output$Download_yandex_search <- downloadHandler(
+    filename <- function() {
+      "Yandex_search.xlsx"
+    },
+    content <- function(file) {
+      write.xlsx(result_ya(), file)
+    }
+  )
 }
 
 shinyApp(ui, server)
